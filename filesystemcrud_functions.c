39a4deaeb6e29e82ebbe7d418a22781c20d23517
JoaoVictorBig156 <joaovirao@gmail.com>
2025-11-26 13:54:53 -0300

    Update filesystemcrud_functions.c

--- filesystemcrud_functions.c
+++ filesystemcrud_functions.c
@@ -18,7 +18,7 @@ char* ler_cha(char* cha_requerido){
     while (!feof(file))
     {
         fgets(leitura_cha,31,file);
-        sscanf(leitura_cha, "codigo:%2s %23[^\n]",codigo,cha_lido);
+        sscanf(leitura_cha, "codigo:%2s Nome do cha:%10[^\n]",codigo,cha_lido);
         if(strcmp(codigo, cha_requerido) == 0){
         break;
         }
