a76b0f7a0bedff7313ff7dfd8d6e162dc5e539c3
JoaoVictorBig156 <joaovirao@gmail.com>
2025-11-26 09:33:30 -0300

    Update filesystemcrud_test.c

--- filesystemcrud_test.c
+++ filesystemcrud_test.c
@@ -2,12 +2,12 @@
 #include <assert.h>
 void main()
 {
-    assert(criar_cha("hibisco", [01])==0)  ;
-    assert(criar_cha("comomila", [02])==0) ;
+    assert(criar_cha("hibisco", 1)==0)  ;
+    assert(criar_cha("comomila", 2)==0) ;
     FILE *file;
     file = fopen("cha.txt", "r");
     assert(file != NULL);
-    char *leitura = ler_cha([01]);
+    char *leitura = ler_cha(1);
     leitura[strcspn(leitura, "\r\n")] = 0;
     assert(strcmp(leitura, "codigo:01 ,Nome do cha:hibisco") == 0);
 }
