8b3e70fac276f09c8a01480cde2ea8305e592f0e
JoaoVictorBig156 <joaovirao@gmail.com>
2025-11-26 09:27:40 -0300

    Update filesystemcrud_functions.c

--- filesystemcrud_functions.c
+++ filesystemcrud_functions.c
@@ -43,7 +43,7 @@ int remover_cha(int* cha_requerido){
         }
         else
         {
-            fprintf(destino,"codigo:%d Nome do cha:%s\n", codigo, cha_lido);
+            fprintf(destino,"codigo:%ls Nome do cha:%s\n", codigo, cha_lido);
         }
     }
     fclose(origem);
